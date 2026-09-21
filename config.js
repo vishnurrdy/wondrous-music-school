113a72de545917c242bcc84365a6406f4389336b
vishnurrdy <vr9309123@gmail.com>
2026-09-21 11:08:26 +0530

    Remove payment configuration

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 window.WONDROUS_CONFIG = {
   SUPABASE_URL: "https://myezereftmiplijfstny.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_-bqY4risRzHhahNyhpqDbw_XysrzqXp",
-  PAYMENT_PROVIDER: "razorpay",
-  RAZORPAY_KEY_ID: "",
   SCHOOL_NAME: "Wondrous Music School"
 };
